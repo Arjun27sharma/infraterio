2cb257065d9a27de2aec4479cf272db574a91b6e
Arjun Sharma <arjunsharma6622@gmail.com>
2023-05-13 02:53:48 +0530

    added product desc mobile

---
+++ Product Description Mobile/script.js
@@ -0,0 +1,103 @@
+
+const circles = document.querySelectorAll('.color_circle');
+
+circles.forEach(circle => {
+  circle.addEventListener('click', () => {
+    // remove "selected" class from all circles
+    circles.forEach(c => {
+      c.classList.remove('selected');
+    });
+    // add "selected" class to clicked circle
+    circle.classList.add('selected');
+  });
+});
+
+const tags = document.querySelectorAll('.product_details_tag');
+
+tags.forEach(tag => {
+  tag.addEventListener('click', () => {
+    tag.classList.toggle('selected');
+  });
+});
+
+
+
+$(document).ready(function() {
+    // First Slider
+    const cards1 = $('#card-container-1 .card_x');
+    const cardWidth1 = cards1.outerWidth(true);
+    const maxPosition1 = -(cardWidth1 * (cards1.length - 3));
+    let currentPosition1 = 0;
+  
+    // Disable left arrow initially
+    $('#left-arrow-1').prop('disabled', true);
+  
+    // Move cards left
+    $('#left-arrow-1').click(function() {
+      if (currentPosition1 < 0) {
+        currentPosition1 += cardWidth1;
+        cards1.css('transform', `translateX(${currentPosition1}px)`);
+        $('#right-arrow-1').prop('disabled', false);
+      }
+      if (currentPosition1 === 0) {
+        $('#left-arrow-1').prop('disabled', true);
+      }
+    });
+  
+    // Move cards right
+    $('#right-arrow-1').click(function() {
+      if (currentPosition1 > maxPosition1) {
+        currentPosition1 -= cardWidth1;
+        cards1.css('transform', `translateX(${currentPosition1}px)`);
+        $('#left-arrow-1').prop('disabled', false);
+      }
+      if (currentPosition1 === maxPosition1) {
+        $('#right-arrow-1').prop('disabled', true);
+      }
+    });
+  
+  
+    // Second Slider
+    const cards2 = $('#card-container-2 .card_x');
+    const cardWidth2 = cards2.outerWidth(true);
+    const maxPosition2 = -(cardWidth2 * (cards2.length - 3));
+    let currentPosition2 = 0;
+  
+    // Disable left arrow initially
+    $('#left-arrow-2').prop('disabled', true);
+  
+    // Move cards left
+    $('#left-arrow-2').click(function() {
+      if (currentPosition2 < 0) {
+        currentPosition2 += cardWidth2;
+        cards2.css('transform', `translateX(${currentPosition2}px)`);
+        $('#right-arrow-2').prop('disabled', false);
+      }
+      if (currentPosition2 === 0) {
+        $('#left-arrow-2').prop('disabled', true);
+      }
+    });
+  
+    // Move cards right
+    $('#right-arrow-2').click(function() {
+      if (currentPosition2 > maxPosition2) {
+        currentPosition2 -= cardWidth2;
+        cards2.css('transform', `translateX(${currentPosition2}px)`);
+        $('#left-arrow-2').prop('disabled', false);
+      }
+      if (currentPosition2 === maxPosition2) {
+        $('#right-arrow-2').prop('disabled', true);
+      }
+    });
+
+  });
+  
+  
+
+
+
+  // Initialize carousel
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000 // Set interval between slides in milliseconds
+})
