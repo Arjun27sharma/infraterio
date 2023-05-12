dec56ff526fb05d0363bf4be1e34969c8d9e45d7
Arjun Sharma <arjunsharma6622@gmail.com>
2023-05-13 02:59:38 +0530

    added product page mobile version

---
+++ Product Mobile/script.js
@@ -0,0 +1,137 @@
+
+$(document).ready(function() {
+  $('.menu-trigger').click(function() {
+    $('.menu-trigger').removeClass('active');
+    $(this).addClass('active');
+    $('.mega-menu').removeClass('menu-open');
+    var target = $(this).data('target');
+    $(target).addClass('menu-open');
+
+    console.log(target)
+  });
+
+  $(document).click(function(event) {
+    if (!$(event.target).closest('.nav-container').length) {
+      $('.menu-trigger').removeClass('active');
+      $('.mega-menu').removeClass('menu-open');
+    }
+  });
+});
+
+
+
+const filter_tabs = document.querySelectorAll('.filters-tab');
+
+filter_tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    filter_tabs.forEach(t => {
+      t.classList.remove('active');
+    });
+    tab.classList.add('active');
+  });
+});
+
+
+
+
+
+
+const circles = document.querySelectorAll('.color_circle');
+
+circles.forEach(circle => {
+  circle.addEventListener('click', () => {
+    // remove "selected" class from all circles
+    circles.forEach(c => {
+      c.classList.remove('selected');
+    });
+    // add "selected" class to clicked circle
+    circle.classList.add('selected');
+  });
+});
+
+
+
+
+const tags = document.querySelectorAll('.product_details_tag');
+
+tags.forEach(tag => {
+  tag.addEventListener('click', () => {
+    tag.classList.toggle('selected');
+  });
+});
+
+
+
+
+
+$(document).ready(function() {
+    // First Slider
+    const cards1 = $('#card-container-1 .card_x');
+    const cardWidth1 = cards1.outerWidth(true);
+    const maxPosition1 = -(cardWidth1 * (cards1.length - 3));
+    let currentPosition1 = 0;
+  
+    // Disable left arrow initially
+    $('#left-arrow-1').prop('disabled', true);
+  
+    // Move cards left
+    $('#left-arrow-1').click(function() {
+      if (currentPosition1 < 0) {
+        currentPosition1 += 2*cardWidth1;
+        cards1.css('transform', `translateX(${currentPosition1}px)`);
+        $('#right-arrow-1').prop('disabled', false);
+      }
+      if (currentPosition1 === 0) {
+        $('#left-arrow-1').prop('disabled', true);
+      }
+    });
+  
+    // Move cards right
+    $('#right-arrow-1').click(function() {
+      if (currentPosition1 > maxPosition1) {
+        currentPosition1 -= 2*cardWidth1;
+        cards1.css('transform', `translateX(${currentPosition1}px)`);
+        $('#left-arrow-1').prop('disabled', false);
+      }
+      if (currentPosition1 === maxPosition1) {
+        $('#right-arrow-1').prop('disabled', true);
+      }
+    });
+  
+  
+    // Second Slider
+    const cards2 = $('#card-container-2 .card_x');
+    const cardWidth2 = cards2.outerWidth(true);
+    const maxPosition2 = -(cardWidth2 * (cards2.length - 3));
+    let currentPosition2 = 0;
+  
+    // Disable left arrow initially
+    $('#left-arrow-2').prop('disabled', true);
+  
+    // Move cards left
+    $('#left-arrow-2').click(function() {
+      if (currentPosition2 < 0) {
+        currentPosition2 += 2*cardWidth2;
+        cards2.css('transform', `translateX(${currentPosition2}px)`);
+        $('#right-arrow-2').prop('disabled', false);
+      }
+      if (currentPosition2 === 0) {
+        $('#left-arrow-2').prop('disabled', true);
+      }
+    });
+  
+    // Move cards right
+    $('#right-arrow-2').click(function() {
+      if (currentPosition2 > maxPosition2) {
+        currentPosition2 -= 2*cardWidth2;
+        cards2.css('transform', `translateX(${currentPosition2}px)`);
+        $('#left-arrow-2').prop('disabled', false);
+      }
+      if (currentPosition2 === maxPosition2) {
+        $('#right-arrow-2').prop('disabled', true);
+      }
+    });
+
+  });
+  
+  
